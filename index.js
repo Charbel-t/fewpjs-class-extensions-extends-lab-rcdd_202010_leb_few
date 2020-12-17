766a1a516a0f6ac12c7f392c3588e29b21c86eb2
Charbel t <charbel2000.52@gmail.com>
2020-12-17 08:30:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,17 @@ class Triangle extends Polygon{
     let side1=this.sides[0];
     let side2=this.sides[1];
     let side3=this.sides[2];
-    return ((side1+side2>side3)&& (side1+side3>side2)&& (side2+side3>side1)
+    return ((side1+side2>side3)&& (side1+side3>side2)&& (side2+side3>side1));
+  }
+}
+
+class Square extends Polygon{
+  get isValid(){
+    if(countSides()!==4)  return;
+    let side1=this.sides[0];
+    let side2=this.sides[1];
+    let side3=this.sides[2];
+    let side4=this.sides[3];
   }
 }
 
